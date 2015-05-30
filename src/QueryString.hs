850021443176e5d2436ba964914681bd4564f9a5
wayofthepie <wayofthepie@gmail.com>
2015-05-30 20:13:48 +0000

    Use stackage, add some comments

--- src/QueryString.hs
+++ src/QueryString.hs
@@ -9,20 +9,29 @@
 module QueryString where
 
 import Control.Applicative hiding (empty)
+import Data.ByteString.Builder
+import Data.ByteString.Conversion
 import qualified Data.ByteString.Lazy.Char8 as BC
 import Data.ByteString.Lazy hiding (foldl, foldl1)
 import Data.Proxy
 import Data.Monoid
 import GHC.TypeLits
 
+{-
+    Toying with well typed query strings for Docker's /images/create
+    endpoint.
+-}
 
-data FromImage = FromImage ByteString deriving Show
+-- | Parameters which can be set
+data FromImage  = FromImage Builder
+data FromSrc    = FromSrc Builder
+data Registry   = Registry Builder
+data Repo       = Repo Builder
+data Tag        = Tag Builder
 
-class ToByteString a where
-    toByteString :: a -> ByteString
 
 instance ToByteString FromImage where
-    toByteString (FromImage bs) = bs
+    builder (FromImage b) = b
 
 type family TypedKey (k :: *) :: Symbol where
     TypedKey FromImage = "fromImage"
@@ -30,8 +39,23 @@ type family TypedKey (k :: *) :: Symbol where
 data QueryKeyVal (k :: Symbol) a :: * where
     QueryKeyVal :: a -> QueryKeyVal (TypedKey a) a
 
-class QueryString a where
+
+
+class ToQueryString a where
     buildQueryString :: a -> ByteString
+
+
+instance (k ~ TypedKey a, KnownSymbol k, ToByteString a) => ToQueryString (QueryKeyVal k a) where
+        buildQueryString (QueryKeyVal a) =
+            (BC.pack $ symbolVal(Proxy :: Proxy k)) <> "=" <> toByteString a
+
+
+instance ToQueryString a => ToQueryString [a] where
+    buildQueryString (x:xs) =
+        foldl (\bs val -> bs <> "&" <> buildQueryString val)
+              (buildQueryString x)
+              xs
+
 
 instance (KnownSymbol k, Show a) => Show (QueryKeyVal k a) where
     show (QueryKeyVal a) = "QueryKeyVal "
@@ -39,14 +63,4 @@ instance (KnownSymbol k, Show a) => Show (QueryKeyVal k a) where
         <> " "
         <> show a
 
-
-instance (k ~ TypedKey a, KnownSymbol k, ToByteString a) => QueryString (QueryKeyVal k a) where
-        buildQueryString (QueryKeyVal a) =
-            (BC.pack $ symbolVal(Proxy :: Proxy k)) <> "=" <> toByteString a
-
-instance QueryString a => QueryString [a] where
-    buildQueryString (x:xs) =
-        foldl (\bs val -> bs <>  "&" <> buildQueryString val)
-              (buildQueryString x)
-              xs
 
